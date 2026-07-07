7b56de25d440793ef9bc51381ababb8f44742fbe
千絮 <13004978148@163.com>
2026-07-08 00:56:47 +0800

    简单配置
    简单配置

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "QCrox",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm QCrox.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,26 +24,8 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
-		},
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
-			showName: false,
-		},
-		{
-			name: "Email",
-			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
-			showName: false,
-		},
-		{
-			name: "RSS",
-			icon: "fa7-solid:rss",
-			url: "/rss/",
-			showName: false,
+			url: "https://qm.qq.com/q/ee38kn0Fyg",
+			showName: true,
 		},
 	],
 };
---
+++ src/content/posts/hello/index.md
@@ -0,0 +1,25 @@
+---
+title: QCrox欢迎你
+published: 2026-07-08
+description: 欢迎页面
+image: ./cover.avif
+tags: [欢迎]
+category: 闲聊
+draft: false
+author: 千絮
+---
+# **欢迎来到揪码博客**
+### **我们是揪码互联网科技公司**，成立于2026.7.1
+我们主营:
+```CN
+网站开发，社区开发，AI
+```
+
+同时我们有一个奴属于本公司的工作室
+叫做YoraStudio
+主营
+
+
+```CN
+校园社区
+```
